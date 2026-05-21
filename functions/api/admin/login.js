5ba0ced516ed6313382bf9e1b3bacfbf20316d50
AYMAN2448 <aloby2448@gmail.com>
2026-05-21 17:16:44 +0200

    Update login.js

--- functions/api/admin/login.js
+++ functions/api/admin/login.js
@@ -4,7 +4,5 @@ export async function onRequestPost({ request, env }) {
   if (!admin || admin.password !== password) {
     return Response.json({ success: false }, { status: 401 });
   }
-  // توليد توكن بسيط
-  const token = 'secret_admin_token'; // يمكن توليده عشوائياً وتخزينه في الجلسة
-  return Response.json({ success: true, token });
+  return Response.json({ success: true, token: 'secret_admin_token' });
 }
